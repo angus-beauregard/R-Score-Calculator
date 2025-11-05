537fa4b0efd0c36b9e49cae27623a7fead788ab6
Angus Beauregard <65871271+angus-beauregard@users.noreply.github.com>
2025-11-05 17:13:56 -0500

    Update Main.py

--- pages/Main.py
+++ pages/Main.py
@@ -123,8 +123,12 @@ if not st.session_state.tos_accepted:
 
             # 👇 if the account is NOT premium, we’ll send them to the payment page
             if not st.session_state.get("is_premium", False):
+                # add the checkout flag so landing shows Stripe
                 st.experimental_set_query_params(checkout="1")
-                st.switch_page(/landing)
+                # go to root landing.py
+                st.switch_page("landing.py")
+                st.stop()
+
 
             st.rerun()
         else:
