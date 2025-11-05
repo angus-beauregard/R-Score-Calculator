49a655c7ee18d3ad6e5e72fa796a26f37e772b4f
Angus Beauregard <65871271+angus-beauregard@users.noreply.github.com>
2025-11-05 17:15:30 -0500

    Update Free.py

--- pages/Free.py
+++ pages/Free.py
@@ -12,7 +12,7 @@ if "upgrade" in qp:
     st.session_state.is_premium = True
     st.session_state.onboarded = True
     # adjust this to the real main/pro app filename
-    st.switch_page(pages/Main)
+    st.switch_page(/pages/Main)
 
 # ---------- BASE STYLES ----------
 st.markdown(
